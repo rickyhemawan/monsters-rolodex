5d783ddad944d6e8623854551eee0fdd5c16a62b
rickyhemawan <rickyhemawan23@gmail.com>
2020-07-14 14:26:47 +0800

    Updates

---
+++ precache-manifest.1127565a605f691873acfef7b89923cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7415c23d1d16e079ad2df84dee8046cd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb3420d7ff6d55a8f09f",
+    "url": "/monsters-rolodex/static/css/main.a4768511.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3420d7ff6d55a8f09f",
+    "url": "/monsters-rolodex/static/js/main.cce01f6a.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
